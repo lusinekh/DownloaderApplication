bb03b902b76b7d231d66c8d81f25f8f088a3471a
User <User@DESKTOP-I87ABNR>
2019-04-13 11:13:26 -0700

    Add BugFix

--- DownloaderApplication/DownloaderApplication/MainWindow.xaml.cs
+++ DownloaderApplication/DownloaderApplication/MainWindow.xaml.cs
@@ -17,43 +17,49 @@ namespace DownloaderApplication
         {
             InitializeComponent();
         }
-
         System.Threading.CancellationTokenSource ctsForDownload;
         private async void DownloadUri_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 DownloadCancel.IsEnabled = true;
-              
+
+                DownloadUri.IsEnabled = false;
 
                 Uri address = new Uri(TextUri.Text);
+
                 string[] ar = address.Segments;
+
                 string EnterFolderName = EnterName.Text;
+
                 string folderPath = System.IO.Path.Combine(
                         Environment.GetFolderPath(Environment.SpecialFolder.Desktop), EnterFolderName);
 
                 Directory.CreateDirectory(folderPath);
+
                 string newFile = folderPath + @"\" + ar[ar.Length - 1];
+
                 using (WebClient client = new WebClient())
                 {
-                    DownloadUri.IsEnabled = false;
                     ctsForDownload = new CancellationTokenSource();
+
                     ctsForDownload.Token.Register(() =>
-                    {                       
+                    {
                         client.CancelAsync();
-                       
+
                     });
+
                     client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgresBarr_ValueChanged);
 
+
                     client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
-                   await client.DownloadFileTaskAsync(address, newFile);
 
+                    await client.DownloadFileTaskAsync(address, newFile);
                 }
-
             }
             catch (UriFormatException ex)
             {
-                MessageBox.Show(ex.Message);              
+                MessageBox.Show(ex.Message);
             }
 
             catch (IOException ex)
@@ -66,28 +72,24 @@ namespace DownloaderApplication
                 throw;
             }
 
-
             finally
             {
                 DownloadUri.IsEnabled = true;
-                DownloadCancel.IsEnabled= false;
+                DownloadCancel.IsEnabled = false;
                 ProgresBarr.Value = 0;
             }
-
         }
-        
-        private void ProgresBarr_ValueChanged(object sender, DownloadProgressChangedEventArgs e )
-        { 
+
+        private void ProgresBarr_ValueChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
             double bytesIn = double.Parse(e.BytesReceived.ToString());
             double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
             double percentage = bytesIn / totalBytes * 100;
             ProgresBarr.Value = int.Parse(Math.Truncate(percentage).ToString());
         }
-
         void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
         {
             MessageBox.Show("Download Completed");
-
         }
         private void DownloadCancel_Click(object sender, RoutedEventArgs e)
         {
